5064f1e9e6bf35e545588a7c9eccf4c1bc1ae7fc
2beta <Cdebetaz@gmail.com>
2018-09-05 22:35:47 +0200

    Add files via upload
    change debounce library

--- SimpleMP3.ino
+++ SimpleMP3.ino
@@ -4,58 +4,116 @@
 
  Project:
  MP3 Simple player base on
- - DFPlayer - A Mini MP3 Player For Arduino
+ - DFPlayer : https://github.com/DFRobot/DFRobotDFPlayerMini
+ - debounce : https://github.com/Mokolea/InputDebounce/
 
  Version:
  00 : Initial release
+ 
 
  ****************************************************/
 
 #include "Arduino.h"
 #include "SoftwareSerial.h"
 #include "DFRobotDFPlayerMini.h"
-#include "Pushbutton.h"
+#include "InputDebounce.h"
 
-// constants won't change. They're used here to set pin numbers ------------------------
-const int button1Pin = 2;     // the number of the pushbutton Playlist 1 pin
-const int button2Pin = 3;     // the number of the pushbutton Playlist 2 pin
-const int button3Pin = 4;     // the number of the pushbutton Playlist 3 pin
-const int button4Pin = 5;     // the number of the pushbutton Playlist 4 pin
-const int button5Pin = 6;     // the number of the pushbutton Playlist 5 pin
-const int button6Pin = 7;     // the number of the pushbutton Playlist 6 pin
-const int buttonVolDecPin = 8;      // the number of the pushbutton Volume - pin
-const int buttonVolIncPin = 9;     // the number of the pushbutton Volume + pin
-const int ledPin =  13;       // the number of the LED pin
+#define BUTTON_DEBOUNCE_DELAY   100   // [ms]
 
-//Declaration ----------------------------------
-SoftwareSerial mySoftwareSerial(10, 11); // RX, TX
-DFRobotDFPlayerMini myDFPlayer;
-void printDetail(uint8_t type, int value);
-int ReadButtonId (void);
-int Volume;            //Volume for Speaker
-Pushbutton button1;
-Pushbutton button2;
-Pushbutton button3;
-Pushbutton button4;
-Pushbutton button5;
-Pushbutton button6;
-Pushbutton buttonVolDec;
-Pushbutton buttonVolInc;
+// Declaration ------------------------------------------------------
+SoftwareSerial mySoftwareSerial(10, 11);    // RX, TX, for communication with DFPLayer
+DFRobotDFPlayerMini myDFPlayer;             // DFplayer
+void printDetail(uint8_t type, int value);  // Print some details of DFplayer module
+int Volume;                                 // Volume for Speaker
+
+const int ledPin = LED_BUILTIN;             // the number of the LED pin// 13;
+static const int pinSwitch1 = 2;
+static const int pinSwitch2 = 3;
+static const int pinSwitch3 = 4;
+static const int pinSwitch4 = 5;
+static const int pinSwitch5 = 6;
+static const int pinSwitch6 = 7;
+static const int pinSwitch7 = 8;            // Volume-
+static const int pinSwitch8 = 9;            // Volume+
+
+static InputDebounce buttonTest1;           // Not enabled yet, setup has to be called first, see setup() below
+static InputDebounce buttonTest2;
+static InputDebounce buttonTest3;
+static InputDebounce buttonTest4;
+static InputDebounce buttonTest5;
+static InputDebounce buttonTest6;
+static InputDebounce buttonTest7;
+static InputDebounce buttonTest8;
+
+//---------------------------------------------------
+void buttonTest_pressedCallback(uint8_t pinIn)
+{
+  switch (pinIn){
+      case 2:
+        myDFPlayer.loopFolder(1); //loop all mp3 files in folder SD:/01.
+        Serial.print ("Folder1");
+        break;
+      case 3:
+        myDFPlayer.loopFolder(2); //loop all mp3 files in folder SD:/02.
+        Serial.print ("Folder2");
+        break;
+      case 4:
+        myDFPlayer.loopFolder(3); //loop all mp3 files in folder SD:/03.
+        Serial.print ("Folder3");
+        break;
+      case 5:
+        myDFPlayer.loopFolder(4); //loop all mp3 files in folder SD:/04.
+        Serial.print ("Folder4");
+        break;
+      case 6:
+        myDFPlayer.loopFolder(5); //loop all mp3 files in folder SD:/05.
+        Serial.print ("Folder5");
+        break;
+      case 7:
+        myDFPlayer.loopFolder(6); //loop all mp3 files in folder SD:/06.
+        Serial.print ("Folder6");
+        break;
+      case 8:
+        myDFPlayer.volume(15); //Volume-
+        Serial.print ("volume-");
+        Serial.println(myDFPlayer.readVolume()); //read current volume
+        break;
+      case 9:
+        myDFPlayer.volume(30); //Volume+
+        Serial.print ("volume+");
+        Serial.println(myDFPlayer.readVolume()); //read current volume1
+        break;
+    }
+}
 
 //-----------------------------------------------------
 void setup()
 {
+  // initialize digital pin as an output
+  pinMode(ledPin, OUTPUT);
+  
   mySoftwareSerial.begin(9600);
   Serial.begin(115200);
 
-  button1.begin(button1Pin, PullUp, 1000, 500);
-  button2.begin(button2Pin, PullUp, 1000, 500);
-  button3.begin(button3Pin, PullUp, 1000, 500);
-  button4.begin(button4Pin, PullUp, 1000, 500);
-  button5.begin(button5Pin, PullUp, 1000, 500);
-  button6.begin(button6Pin, PullUp, 1000, 500);
-  buttonVolDec.begin(buttonVolDecPin, PullUp, 1000, 500);
-  buttonVolInc.begin(buttonVolIncPin, PullUp, 1000, 500);
+  // register callback functions (shared, used by all buttons)
+  buttonTest1.registerCallbacks(buttonTest_pressedCallback, NULL, NULL, NULL);
+  buttonTest2.registerCallbacks(buttonTest_pressedCallback, NULL, NULL, NULL);
+  buttonTest3.registerCallbacks(buttonTest_pressedCallback, NULL, NULL, NULL);
+  buttonTest4.registerCallbacks(buttonTest_pressedCallback, NULL, NULL, NULL);
+  buttonTest5.registerCallbacks(buttonTest_pressedCallback, NULL, NULL, NULL);
+  buttonTest6.registerCallbacks(buttonTest_pressedCallback, NULL, NULL, NULL);
+  buttonTest7.registerCallbacks(buttonTest_pressedCallback, NULL, NULL, NULL);
+  buttonTest8.registerCallbacks(buttonTest_pressedCallback, NULL, NULL, NULL);  
+  
+  // setup input buttons (debounced)
+  buttonTest1.setup(pinSwitch1, BUTTON_DEBOUNCE_DELAY, InputDebounce::PIM_INT_PULL_UP_RES);
+  buttonTest2.setup(pinSwitch2, BUTTON_DEBOUNCE_DELAY, InputDebounce::PIM_INT_PULL_UP_RES);
+  buttonTest3.setup(pinSwitch3, BUTTON_DEBOUNCE_DELAY, InputDebounce::PIM_INT_PULL_UP_RES);
+  buttonTest4.setup(pinSwitch4, BUTTON_DEBOUNCE_DELAY, InputDebounce::PIM_INT_PULL_UP_RES);
+  buttonTest5.setup(pinSwitch5, BUTTON_DEBOUNCE_DELAY, InputDebounce::PIM_INT_PULL_UP_RES);
+  buttonTest6.setup(pinSwitch6, BUTTON_DEBOUNCE_DELAY, InputDebounce::PIM_INT_PULL_UP_RES);
+  buttonTest7.setup(pinSwitch7, BUTTON_DEBOUNCE_DELAY, InputDebounce::PIM_INT_PULL_UP_RES);
+  buttonTest8.setup(pinSwitch8, BUTTON_DEBOUNCE_DELAY, InputDebounce::PIM_INT_PULL_UP_RES);
     
   Serial.println();
   Serial.println(F("Simple MP3 for my children"));
@@ -76,7 +134,7 @@ void setup()
   myDFPlayer.outputDevice(DFPLAYER_DEVICE_SD);//----Set device we use SD as default----
 
   //----Read imformation----
-  Serial.println ("DFplayer Status");
+  Serial.println ("DFplayer Status -------------------");
   Serial.println(myDFPlayer.readState()); //read mp3 state
   Serial.println(myDFPlayer.readVolume()); //read current volume
   Serial.println(myDFPlayer.readEQ()); //read EQ setting
@@ -93,118 +151,29 @@ void setup()
 //------------------------------------------------------------
 void loop()
 {
-
-  switch (ReadButtonId()){
-    case 1:
-      myDFPlayer.loopFolder(1); //loop all mp3 files in folder SD:/01.
-      Serial.print ("Folder1");
-      break;
-    case 2:
-      myDFPlayer.loopFolder(2); //loop all mp3 files in folder SD:/02.
-      Serial.print ("Folder2");
-      break;
-    case 3:
-      myDFPlayer.loopFolder(3); //loop all mp3 files in folder SD:/03.
-      Serial.print ("Folder3");
-      break;
-    case 4:
-      myDFPlayer.loopFolder(4); //loop all mp3 files in folder SD:/04.
-      Serial.print ("Folder4");
-      break;
-    case 5:
-      myDFPlayer.loopFolder(5); //loop all mp3 files in folder SD:/05.
-      Serial.print ("Folder5");
-      break;
-    case 6:
-      myDFPlayer.loopFolder(6); //loop all mp3 files in folder SD:/06.
-      Serial.print ("Folder6");
-      break;
-    case 10:
-      myDFPlayer.volume(15); //Volume-
-      Serial.print ("volume-");
-      Serial.println(myDFPlayer.readVolume()); //read current volume
-      break;
-    case 11:
-      myDFPlayer.volume(30); //Volume+
-      Serial.print ("volume+");
-      Serial.println(myDFPlayer.readVolume()); //read current volume1
-      break;
-  }
+  //-------------------------------------------------------!!!!!
+  unsigned long now = millis();
+  
+  // poll button state
+  buttonTest1.process(now); // callbacks called in context of this function
+  buttonTest2.process(now);
+  buttonTest3.process(now); // callbacks called in context of this function
+  buttonTest4.process(now);
+  buttonTest5.process(now); // callbacks called in context of this function
+  buttonTest6.process(now);
+  buttonTest7.process(now); // callbacks called in context of this function
+  buttonTest8.process(now);
+  delay(1); // [ms]
+  //---------------------------------------------------!!!!!
   
   if (myDFPlayer.available()) {
     printDetail(myDFPlayer.readType(), myDFPlayer.read()); //Print the detail message from DFPlayer to handle different errors and states.
   }
+
+  digitalWrite(ledPin, HIGH); // turn the LED on
 }
 
-//----Read if button is pressed and his id
-int ReadButtonId (void){
-  int sensorVal=0; 
 
-  if (button1.update()==ButtonStateHold){
-    sensorVal=1;
-  }
-  else if (button2.update()==ButtonStateHold){
-    sensorVal=2;
-  }
-  else if (button3.update()==ButtonStateHold){
-    sensorVal=3;
-  }
-  else if (button4.update()==ButtonStateHold){
-    sensorVal=4;
-  }
-  else if (button5.update()==ButtonStateHold){
-    sensorVal=5;
-  }
-  else if (button6.update()==ButtonStateHold){
-    sensorVal=6;
-  }
-  else if (buttonVolDec.update()==ButtonStateHold){
-    sensorVal=10;
-  }
-  else if (buttonVolInc.update()==ButtonStateHold){
-    sensorVal=11;
-  }
-
-  //----------------------DEBUG-------------
-  /*
-  int inByte=0;
-  if (Serial.available() > 0) {
-    // get incoming byte:
-    inByte=(Serial.read());
-    switch (inByte) {
-      case ('1'):
-        sensorVal=1;
-        Serial.write("un");
-        break;
-      case ('2'):
-        sensorVal=2;
-        Serial.write("deux");
-        break;
-      case ('3'):
-        sensorVal=3;
-        Serial.write("trois");
-        break;
-      case ('4'):
-        sensorVal=4;
-        Serial.write("quatre");
-        break;
-      case ('5'):
-        sensorVal=5;
-        Serial.write("cinq");
-        break;
-      case ('8'):
-        sensorVal=10;
-        Serial.write("vol-");
-        break;
-      case ('9'):
-        sensorVal=11;
-        Serial.write("vol+");
-        break;
-    }
-  }*/
-  //----------------------END DEBUG------------- 
-  return sensorVal;
-}
 
 //----Read status on DFplayer----
 void printDetail(uint8_t type, int value){
